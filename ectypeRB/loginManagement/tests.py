8cc57ab1f185a31c7cb6f5a918733da53947a39c
jyeho <jyeho@gmail.com>
2023-10-06 16:21:52 +0800

    正在编写测试代码

--- ectypeRB/loginManagement/tests.py
+++ ectypeRB/loginManagement/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import json
+# from loginManagement.models import User
 
 # Create your tests here.
+# class LoginManagementTestCase(TestCase):
+#     def setUp(self) -> None:
+#         user_data = {
+#             "username": "defaultTest_user",
+#             "email": "cpdd@gmail.com",
+#             "password": 12121212
+#         }
+#         User.objects.create(**user_data)
+
+#     def test_user_can_login(self):
+
+
+class LoginManagementTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_user_can_register(self):
+        data_json = json.dumps({
+            "username": "defaultTest_user",
+            "email": "cpdd@gmail.com",
+            "password": 12121212
+        })
+        res = self.client.post(
+            "/loginManagement/register/",
+            data=data_json,
+            content_type="application/json"
+        )
+
+        res_data = json.loads(res.content)
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(res_data["info"], "创建用户成功!")
+
+    def test_user_can_login(self):
+        data_json = json.dumps({
+            "username": "defaultTest_user",
+            "password": 12121212
+        })
+        res = self.client.post(
+            "/loginManagament/login/",
+            data=data_json,
+            content_type="application/json"
+        )
+
+        res_data = json.loads(res.content)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res_data["data"]["username"], "defaultTest_user")
