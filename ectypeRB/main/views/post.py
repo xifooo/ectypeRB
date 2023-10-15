9cda24aaa4b237d6e867e21fbd54d411064fd1f6
jyeho <jyeho@gmail.com>
2023-10-15 16:26:29 +0800

    修复了view函数中的类型错误

--- ectypeRB/main/views/post.py
+++ ectypeRB/main/views/post.py
@@ -1,6 +1,7 @@
 import json
 from django.http import JsonResponse
-from main.models import Post, Image
+import main.models as models
+from PIL import Image
 from utils.aux import (
     convert_timezone,
     image_serialize,
@@ -17,7 +18,9 @@ from django.core.files.storage import default_storage
 def post_upload(req, *args, **kwargs):
     payload = kwargs.get("payload")
     user_id = payload.get("user-id")
-    data = json.loads(req.body)
+    # data = json.loads(req.body)
+    form_data = req.POST.get("form-data")
+    data = json.loads(form_data)
     if not data.get("title"):
         return JsonResponse({"error": "the post's title missing"}, status=400)
     post_data = {
@@ -25,12 +28,13 @@ def post_upload(req, *args, **kwargs):
         "content": data.get("content"),
         "user_id": int(user_id)
     }
-    post = Post.objects.create(**post_data)
+    post = models.Post.objects.create(**post_data)
     if not post:
         return JsonResponse({"error": "错误操作"}, status=500)
 
     files = req.FILES.getlist("file")
-    if not len(files):
+    # files = req.POST.get("file")
+    if not files:
         return JsonResponse({
             "data": {
                 "post": {
@@ -56,9 +60,9 @@ def post_upload(req, *args, **kwargs):
             "height": img_height,
             "width": img_width
         }
-        Image.objects.create(**img_data)
-        default_storage(f_path, f)
-    imgs = Image.objects.filter(post_id=post.id).all()
+        models.Image.objects.create(**img_data)
+        default_storage.save(f_path, f)
+    imgs = models.Image.objects.filter(post_id=post.id).all()
     serialied_imgs = image_serialize(imgs)
     return JsonResponse({
         "data": {
@@ -90,15 +94,18 @@ def post_upload(req, *args, **kwargs):
 # GET
 def post_detail(req, *args, **kwargs):
     post_id = req.GET.get("post_id")
-    post = Post.objects.filter(id=int(post_id)).first()
-    imgs = Image.objects.filter(post_id=int(post_id)).all()
-    if not len(imgs):
-        post_data = {
-            "id": post_id,
-            "title": post.title,
-            "content": post.content,
-            "created_at": convert_timezone(post.created_at, TIME_ZONE)
-        }
+    post = models.Post.objects.filter(id=int(post_id)).first()
+    if not post:
+        return JsonResponse({"error": "objective post not found"}, status=404)
+    imgs = models.Image.objects.filter(post_id=int(post_id)).all()
+    post_data = {
+        "id": post_id,
+        "title": post.title,
+        "content": post.content,
+        "created_at": convert_timezone(post.created_at, TIME_ZONE)
+    }
+    if not imgs:
+        return JsonResponse({"data": {"post": {**post_data}}}, status=200)
     serialized_imgs = image_serialize(imgs)
     return JsonResponse({
         "data": {
@@ -115,32 +122,35 @@ def post_flow(req, *args, **kwargs):
     payload = kwargs.get("payload")
     user_id = payload.get("user-id")
     offset, limit = req.GET.get("offset") or 0, req.GET.get("limit") or 10
-    posts = Post.objects.all()
+    posts = models.Post.objects.all()
     limited_posts = limit_querySet(posts, offset=int(offset), limit=int(limit))
-    serialized_posts = posts_serialize(limited_posts)
+    serialized_posts = list(posts_serialize(limited_posts))
     return JsonResponse({
         "data": {
             "post": serialized_posts
         }
     }, status=200)
-    
-    
-# DELETE xxx
-# 删 post ：1.删内容 2.删文件 
+
+
+# DELETE
+# 删 post ：1.删内容 2.删文件
 def post_delete(req, *args, **kwargs):
     post_id = req.GET.get("post_id")
     if not post_id:
         return JsonResponse({"error": "params post_id missing"}, status=400)
-    
-    post = Post.objects.filter(id=int(post_id)).first()
+
+    post = models.Post.objects.filter(id=int(post_id)).first()
     if not post:
         return JsonResponse({"error": "objective post not found"}, status=404)
-    
+
     payload = kwargs.get("payload")
     user_id = payload.get("user-id")
     if post.user.id != int(user_id):
         return JsonResponse({"error": "you are not the post owner"}, status=401)
-    # xxx
-    post.user.remove(user_id)
-    post.save()
+    # 先删文件, 再删数据内容
+    files_path = f"{MEDIA_ROOT}/post/{post.id}"
+    if os.path.isdir(files_path):
+        # os.rmdir(files_path)
+        os.system(f"rm -rf {files_path}")
+    post.delete()
     return JsonResponse({"info": "successfully delete the post"}, status=200)
